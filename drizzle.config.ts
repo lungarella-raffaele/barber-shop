bbd4f52b5578c491417fe0fa2b9b4927826bce5e
Raffaele Lungarella <lungarellaraffaele@gmail.com>
2024-12-08 15:08:20 +0100

    chore: turso and drizzle setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,27 @@
+// import { defineConfig } from 'drizzle-kit';
+// if (!process.env.TURSO_CONNECTION_URL) throw new Error('DATABASE_URL is not set');
+
+// export default defineConfig({
+// 	schema: './src/lib/server/db/schema.ts',
+// 	out: './migrations',
+// 	dialect: 'sqlite',
+// 	dbCredentials: {
+// 		url: process.env.TURSO_CONNECTION_URL!,
+// 		authToken: process.env.TURSO_AUTH_TOKEN!
+// 	}
+// });
+
+import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
+config({ path: '.env' });
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
-
+	out: './migrations',
+	dialect: 'turso',
 	dbCredentials: {
-		url: process.env.DATABASE_URL
-	},
-
-	verbose: true,
-	strict: true,
-	dialect: 'sqlite'
+		url: process.env.TURSO_CONNECTION_URL!,
+		authToken: process.env.TURSO_AUTH_TOKEN!
+	}
 });
