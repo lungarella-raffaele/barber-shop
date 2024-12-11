2dd5d849c4e195513741ce0677a7d2d9fa728205
Raffaele Lungarella <lungarellaraffaele@gmail.com>
2024-12-11 22:01:00 +0100

    feat: added dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,30 @@
 <script lang="ts">
 	import '../app.css';
+	import * as Menubar from '$lib/components/ui/menubar/index.js';
+	import { ModeWatcher } from 'mode-watcher';
+	import Modeswitcher from '$lib/components/app/modeswitcher.svelte';
+
 	let { children } = $props();
 </script>
+
+<ModeWatcher />
+<Menubar.Root>
+	<Menubar.Menu>
+		<Menubar.Trigger>File</Menubar.Trigger>
+		<Menubar.Content>
+			<Menubar.Item>
+				New Tab
+				<Menubar.Shortcut>⌘T</Menubar.Shortcut>
+			</Menubar.Item>
+			<Menubar.Item>New Window</Menubar.Item>
+			<Menubar.Separator />
+			<Menubar.Item>Share</Menubar.Item>
+			<Menubar.Separator />
+			<Menubar.Item>Print</Menubar.Item>
+		</Menubar.Content>
+	</Menubar.Menu>
+
+	<Modeswitcher />
+</Menubar.Root>
 
 {@render children()}
