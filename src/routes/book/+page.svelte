33ced34dcc0082416c03e2d8de3c04337cdec876
Raffaele Lungarella <lungarellaraffaele@gmail.com>
2024-12-11 21:43:49 +0100

    feat: added shadcn dependency

---
+++ src/routes/book/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import BookForm from './BookForm.svelte';
+
+	let { data }: { data: PageData } = $props();
+</script>
+
+<div class="p-20">
+	<BookForm data={data.form} />
+</div>
