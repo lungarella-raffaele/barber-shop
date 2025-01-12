f24b687d79c53172fa3c7c915c5367f90c609948
Raffaele Lungarella <lungarellaraffaele@gmail.com>
2025-01-12 11:02:02 +0100

    feat: enhanced profile page and menubar

--- src/lib/components/app/profile.svelte
+++ src/lib/components/app/profile.svelte
@@ -1,35 +1,73 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import { Label } from '$lib/components/ui/label/index.js';
-	import { Separator } from '$lib/components/ui/separator/index';
 	import type { User } from '$lib/server/db/schema';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
 
 	let { user }: { user: User } = $props();
+
+	let enableModify = $state(false);
 </script>
 
-<Separator />
-<Label>Nome</Label>
-{@render Info(user.firstName)}
+<h1 class="title">Profilo</h1>
+<Card.Root class="mb-5">
+	<Card.Header>
+		<Card.Title>Informazioni personali</Card.Title>
+		<Card.Description>Queste sono le tue informazioni personali</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		<form>
+			<div class="grid w-full items-center gap-4">
+				<div class="flex flex-col space-y-1.5">
+					<Label for="name">Nome</Label>
+					<Input
+						id="name"
+						value={user.firstName ? user.firstName : 'Nessuna informazione'}
+						placeholder="Il tuo nome"
+						disabled={!enableModify}
+					/>
 
-<Label>Cognome</Label>
-{@render Info(user.lastName)}
+					<Label for="name">Cognome</Label>
+					<Input
+						id="name"
+						value={user.lastName ? user.lastName : 'Nessuna informazione'}
+						placeholder="Il tuo cognome"
+						disabled={!enableModify}
+					/>
 
-<Separator />
+					<Label for="name">Numero di telefono</Label>
+					<Input
+						id="name"
+						value={user.phoneNumber ? user.phoneNumber : 'Nessuna informazione'}
+						placeholder="Il tuo numero di cellulare"
+						disabled={!enableModify}
+					/>
 
-<Label>Email</Label>
-{@render Info(user.email)}
-
-<form method="post" action="?/logout" use:enhance>
-	<Button variant="destructive" type="submit">Log out</Button>
-</form>
-
-{#snippet Info(str: string | null)}
-	<span class="text-sm text-muted-foreground">
-		{#if str}
-			{str}
+					<Label for="name">Email</Label>
+					<Input id="name" value={user.email} placeholder="La tua email" disabled={!enableModify} />
+				</div>
+			</div>
+		</form>
+	</Card.Content>
+	<Card.Footer class="flex justify-between">
+		{#if enableModify}
+			<Button onclick={() => (enableModify = false)} variant="outline">Cancel</Button>
+			<Button>Salva</Button>
 		{:else}
-			Not inserted
+			<Button onclick={() => (enableModify = true)}>Modifica</Button>
 		{/if}
-	</span>
-{/snippet}
+	</Card.Footer>
+</Card.Root>
+
+<Card.Root class="border-destructive">
+	<Card.Header>
+		<Card.Title>Elimina account</Card.Title>
+		<Card.Description>
+			Tutte le informazioni relative al tuo profilo saranno eliminate, l'azione non è reversibile.
+		</Card.Description>
+	</Card.Header>
+	<Card.Content class="mt-4 rounded-b-lg border-t border-destructive bg-red-300 bg-opacity-15 py-4">
+		<Button variant="destructive" type="submit">Elimina account</Button>
+	</Card.Content>
+</Card.Root>
