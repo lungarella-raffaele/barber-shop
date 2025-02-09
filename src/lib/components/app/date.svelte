56de82a0cefc8216cac5aad28b1562bdbd3efd7c
Raffaele Lungarella <lungarellaraffaele@gmail.com>
2025-02-09 19:29:01 +0100

    feat: added animation to new reservation slotpicker

--- src/lib/components/app/date.svelte
+++ src/lib/components/app/date.svelte
@@ -1,5 +1,6 @@
 <script>
 	import ReservationManager from '$lib/composables/reservation-manager.svelte';
+	import { fly } from 'svelte/transition';
 	import DatePicker from './datepicker.svelte';
 	import SlotPicker from './slotpicker.svelte';
 
@@ -7,6 +8,18 @@
 </script>
 
 <DatePicker bind:value={reservationManager.date} />
-{#if reservationManager.date}
-	<SlotPicker />
-{/if}
+
+<!-- The grid is used to make the elements live in the same place thus avoiding glithes when animating -->
+<div class="grid">
+	{#if reservationManager.date}
+		{#key reservationManager.date}
+			<div
+				class="col-span-full row-span-full"
+				in:fly={{ x: 80, delay: 50 }}
+				out:fly={{ x: -80 }}
+			>
+				<SlotPicker />
+			</div>
+		{/key}
+	{/if}
+</div>
