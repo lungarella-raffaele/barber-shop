5eb4d0b0bbc6bc9f6d25b9566bbb7bfa124ae132
Raffaele Lungarella <lungarellaraffaele@gmail.com>
2025-02-09 16:28:28 +0100

    feat: users can now delete their account, among all reservations and sessions

--- src/lib/server/backend/user-service.ts
+++ src/lib/server/backend/user-service.ts
@@ -1,6 +1,9 @@
 import { db } from '$lib/server/db';
+import * as auth from '$lib/server/auth';
+import { type RequestEvent, redirect } from '@sveltejs/kit';
 import { eq } from 'drizzle-orm';
 import * as table from '$lib/server/db/schema';
+import { deleteAllReservationsOfUser } from './reservation-service';
 
 export async function insertUser(user: table.User) {
 	const email = user.email.toLowerCase();
@@ -20,4 +23,19 @@ export async function getUser(email: string) {
 
 export async function insertSession(session: table.Session) {
 	return await db.insert(table.session).values(session);
+}
+
+export async function deleteAccount(user: table.User) {
+	await deleteAllSessionOfUser(user.id);
+	await deleteAllReservationsOfUser(user.email);
+	return await db.delete(table.user).where(eq(table.user.id, user.id)).returning().get();
+}
+
+export async function logout(id: string, event: RequestEvent) {
+	await auth.invalidateSession(id);
+	auth.deleteSessionTokenCookie(event);
+}
+
+async function deleteAllSessionOfUser(id: string) {
+	return await db.delete(table.session).where(eq(table.session.userID, id));
 }
--- src/routes/(protected)/profile/+page.server.ts
+++ src/routes/(protected)/profile/+page.server.ts
@@ -1,6 +1,7 @@
 import { fail, redirect } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
-import * as auth from '$lib/server/auth';
+import { logger } from '$lib/server/logger';
+import { deleteAccount, logout } from '$lib/server/backend/user-service';
 
 export const load: PageServerLoad = ({ locals }) => {
 	return { user: locals.user, title: 'Profilo | ' };
@@ -8,13 +9,35 @@ export const load: PageServerLoad = ({ locals }) => {
 
 export const actions: Actions = {
 	logout: async (event) => {
-		console.log('Logging out...');
+		logger.info('Logging out...');
 		if (!event.locals.session) {
 			return fail(401);
 		}
-		await auth.invalidateSession(event.locals.session.id);
-		auth.deleteSessionTokenCookie(event);
+
+		await logout(event.locals.session.id, event);
 
 		return redirect(302, '/');
+	},
+	deleteAccount: async (event) => {
+		const user = event.locals.user;
+		const session = event.locals.session;
+
+		if (!user || !session) {
+			logger.warn('Cannot delete account');
+			return fail(404);
+		}
+
+		logger.warn('Deleting account of user: ' + user.email);
+		const res = await deleteAccount(user);
+
+		if (res) {
+			logger.info('Successfully deleted account of user: ' + res.email);
+			await logout(session.id, event);
+
+			return redirect(302, '/');
+		} else {
+			logger.error('Error while deleting account of user: ' + user.email);
+			return fail(500);
+		}
 	}
 };
