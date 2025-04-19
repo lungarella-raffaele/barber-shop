6cc7bc98e9c72be080ec6642ff9ea65fddb4262b
Raffaele Lungarella <lungarellaraffaele@gmail.com>
2025-04-19 12:41:39 +0200

    feat: added unavailable site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 	import Banner from '$lib/components/app/banner.svelte';
 	import CookieBanner from '$lib/components/app/cookiebanner.svelte';
 	import Footer from '$lib/components/app/footer.svelte';
+	import Logo from '$lib/components/app/logo.svelte';
 	import Modeswitcher from '$lib/components/app/modeswitcher.svelte';
 	import { CircleUser, Citrus } from '$lib/components/icons/index';
 	import { Button, buttonVariants } from '$lib/components/ui/button/index';
@@ -14,87 +15,107 @@
 	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
 
+	import { Instagram } from '$lib/components/icons/index';
+	import { BARBER_SHOP_DETAILS } from '$lib/constants';
+
 	const { data, children } = $props();
 
 	const isLogged = $derived(data.user !== null);
 	const isAdmin = $derived(data.user?.isAdmin);
 
 	let logoutForm: HTMLFormElement | undefined = $state();
+
+	const siteUnavailable = true;
 </script>
 
 <svelte:head>
 	<title>{page.data.title} Emi Hair Club</title>
 </svelte:head>
 
-<ModeWatcher />
-<Toaster richColors position="top-center" />
+{#if siteUnavailable}
+	<div class="p-40">
+		<Logo />
+		<div class="flex flex-col">
+			<p class="mb-4 text-center text-2xl font-bold">Il sito sarà presto disponibile</p>
 
-<Sidebar.Provider>
-	<AppSidebar {isLogged} {isAdmin} />
-	<main class="relative flex h-screen w-full flex-col">
-		<div class="sticky top-0 z-50 flex items-center justify-between border-b bg-background p-3">
-			<!-- Leading -->
-			<Sidebar.Trigger />
-			<!-- Trailing -->
-			<div class="flex items-center">
-				{#if !data.user}
-					<Button href="/login" variant="ghost">Login</Button>
-				{:else}
-					{#if data.user.isAdmin}
-						<Button href="/dashboard" variant="ghost">
-							<Citrus />
-						</Button>
+			<div class="text-center">
+				<Button href={BARBER_SHOP_DETAILS.instagram}><Instagram /> Instagram</Button>
+			</div>
+		</div>
+	</div>
+{:else}
+	<ModeWatcher />
+	<Toaster richColors position="top-center" />
+
+	<Sidebar.Provider>
+		<AppSidebar {isLogged} {isAdmin} />
+		<main class="relative flex h-screen w-full flex-col">
+			<div
+				class="sticky top-0 z-50 flex items-center justify-between border-b bg-background p-3"
+			>
+				<!-- Leading -->
+				<Sidebar.Trigger />
+				<!-- Trailing -->
+				<div class="flex items-center">
+					{#if !data.user}
+						<Button href="/login" variant="ghost">Login</Button>
+					{:else}
+						{#if data.user.isAdmin}
+							<Button href="/dashboard" variant="ghost">
+								<Citrus />
+							</Button>
+						{/if}
+						<DropdownMenu.Root>
+							<DropdownMenu.Trigger class={buttonVariants({ variant: 'ghost' })}>
+								<CircleUser />
+							</DropdownMenu.Trigger>
+							<DropdownMenu.Content>
+								<DropdownMenu.Group class="flex flex-col">
+									<Button
+										class="m-0 justify-start p-2 text-right no-underline hover:bg-muted hover:no-underline"
+										variant="link"
+										href="/profile">Profilo</Button
+									>
+									<Button
+										class="m-0 justify-start p-2 text-right no-underline hover:bg-muted hover:no-underline"
+										variant="link"
+										href="profile/myreservations">Prenotazioni</Button
+									>
+									<DropdownMenu.Separator />
+
+									<DropdownMenu.Item
+										class="text-destructive"
+										onSelect={() => logoutForm?.requestSubmit()}
+									>
+										Esci
+									</DropdownMenu.Item>
+								</DropdownMenu.Group>
+							</DropdownMenu.Content>
+						</DropdownMenu.Root>
 					{/if}
-					<DropdownMenu.Root>
-						<DropdownMenu.Trigger class={buttonVariants({ variant: 'ghost' })}>
-							<CircleUser />
-						</DropdownMenu.Trigger>
-						<DropdownMenu.Content>
-							<DropdownMenu.Group class="flex flex-col">
-								<Button
-									class="m-0 justify-start p-2 text-right no-underline hover:bg-muted hover:no-underline"
-									variant="link"
-									href="/profile">Profilo</Button
-								>
-								<Button
-									class="m-0 justify-start p-2 text-right no-underline hover:bg-muted hover:no-underline"
-									variant="link"
-									href="profile/myreservations">Prenotazioni</Button
-								>
-								<DropdownMenu.Separator />
-
-								<DropdownMenu.Item
-									class="text-destructive"
-									onSelect={() => logoutForm?.requestSubmit()}
-								>
-									Esci
-								</DropdownMenu.Item>
-							</DropdownMenu.Group>
-						</DropdownMenu.Content>
-					</DropdownMenu.Root>
-				{/if}
-				<Modeswitcher />
+					<Modeswitcher />
+				</div>
 			</div>
-		</div>
 
-		<div class="flex min-h-0 flex-1 flex-col">
-			<div class="flex-1 p-3">
-				{#if data.banner?.visible}
-					<Banner message={data.banner.message} />
-				{/if}
-				{@render children()}
+			<div class="flex min-h-0 flex-1 flex-col">
+				<div class="flex-1 p-3">
+					{#if data.banner?.visible}
+						<Banner message={data.banner.message} />
+					{/if}
+					{@render children()}
+				</div>
+				<Footer />
 			</div>
-			<Footer />
-		</div>
-	</main>
-</Sidebar.Provider>
+		</main>
+	</Sidebar.Provider>
 
-<CookieBanner />
+	<CookieBanner />
 
-<form
-	bind:this={logoutForm}
-	method="post"
-	action="/profile?/logout"
-	use:enhance
-	id="logout-menubar-form"
-></form>
+	<form
+		bind:this={logoutForm}
+		method="post"
+		action="/profile?/logout"
+		use:enhance
+		id="logout-menubar-form"
+	></form>
+{/if}
